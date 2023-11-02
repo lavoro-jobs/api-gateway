3317f5d4ec2d80f72863bc6f943071b4f6012d76
Marko Bolt <marko.bolt@fer.hr>
2023-11-02 19:12:45 +0100

    changed syntax for new library database implementation

--- lavoro_api_gateway/database/queries.py
+++ lavoro_api_gateway/database/queries.py
@@ -5,25 +5,30 @@ from lavoro_library.models import PositionCatalog, EducationCatalog, ContractTyp
 
 
 def get_position_catalog() -> List[PositionCatalog]:
-    result = db.execute_query("SELECT * FROM position_catalog")
+    query_tuple = ("SELECT * FROM position_catalog", None)
+    result = db.execute_one(query_tuple)
     return [PositionCatalog(**position) for position in result["result"]]
 
 
 def get_skills_catalog() -> List[SkillsCatalog]:
-    result = db.execute_query("SELECT * FROM skills_catalog")
+    query_tuple = ("SELECT * FROM skills_catalog", None)
+    result = db.execute_one(query_tuple)
     return [SkillsCatalog(**skill) for skill in result["result"]]
-    
+
 
 def get_education_catalog() -> List[EducationCatalog]:
-    result = db.execute_query("SELECT * FROM education_catalog")
+    query_tuple = ("SELECT * FROM education_catalog", None)
+    result = db.execute_one(query_tuple)
     return [EducationCatalog(**education) for education in result["result"]]
-    
+
 
 def get_contract_type_catalog() -> List[ContractTypeCatalog]:
-    result = db.execute_query("SELECT * FROM contract_type_catalog")
+    query_tuple = ("SELECT * FROM contract_type_catalog", None)
+    result = db.execute_one(query_tuple)
     return [ContractTypeCatalog(**contract_type) for contract_type in result["result"]]
-    
+
 
 def get_work_type_catalog() -> List[WorkTypeCatalog]:
-    result = db.execute_query("SELECT * FROM work_type_catalog")
+    query_tuple = ("SELECT * FROM work_type_catalog", None)
+    result = db.execute_one(query_tuple)
     return [WorkTypeCatalog(**work_type) for work_type in result["result"]]
