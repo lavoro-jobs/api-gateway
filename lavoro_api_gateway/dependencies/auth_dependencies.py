88d68731a3f7e980ad60c8044d092ed7905fd28f
Marko Bolt <marko.bolt@fer.hr>
2023-11-06 22:03:07 +0100

    changed to use LoginForm model

--- lavoro_api_gateway/dependencies/auth_dependencies.py
+++ lavoro_api_gateway/dependencies/auth_dependencies.py
@@ -2,16 +2,51 @@ import os
 
 from typing import Annotated
 
+from typing import Optional, Dict
+
 from jose import JWTError, jwt
 
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status, Request
+from fastapi.security import OAuth2
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.utils import get_authorization_scheme_param
 
 
 from lavoro_api_gateway.helpers.auth_helpers import get_account
 from lavoro_library.models import User, TokenData
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+class OAuth2PasswordBearerWithCookie(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.cookies.get("access_token")
+        print("access_token is", authorization)
+
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
+
+
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/api/v1/auth/login")
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALGORITHM = "HS256"
--- lavoro_api_gateway/helpers/auth_helpers.py
+++ lavoro_api_gateway/helpers/auth_helpers.py
@@ -1,10 +1,9 @@
 import requests
 
 from fastapi.encoders import jsonable_encoder
-from fastapi.security import OAuth2PasswordRequestForm
 
 from lavoro_api_gateway.helpers.request_helpers import propagate_response
-from lavoro_library.models import UserInDB, Token, RegistrationForm
+from lavoro_library.models import LoginForm, RegistrationForm, Token, UserInDB
 
 
 def get_account(email: str):
@@ -12,7 +11,7 @@ def get_account(email: str):
     return propagate_response(response, response_model=UserInDB)
 
 
-def get_token(form_data: OAuth2PasswordRequestForm):
+def get_token(form_data: LoginForm):
     response = requests.post("http://auth-api/login/token", data=jsonable_encoder(form_data))
     return propagate_response(response, response_model=Token)
 
