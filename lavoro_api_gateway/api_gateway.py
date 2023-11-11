b48b6d29455b4f8b16208682d54006dadb81a2db
Your Correct Name <jakov.vinozganic@fer.hr>
2023-11-11 12:21:20 +0100

    Added * to origins

--- lavoro_api_gateway/api_gateway.py
+++ lavoro_api_gateway/api_gateway.py
@@ -6,13 +6,9 @@ from lavoro_api_gateway.routers.config import router as config_router
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
