fc88f95a21e18fef0fc742883166f541e09acab1
Marko Bolt <marko.bolt@fer.hr>
2023-12-06 23:29:19 +0100

    Changed models organization

--- lavoro_api_gateway/helpers/auth_helpers.py
+++ lavoro_api_gateway/helpers/auth_helpers.py
@@ -3,25 +3,28 @@ import requests
 from fastapi.encoders import jsonable_encoder
 
 from lavoro_api_gateway.helpers.request_helpers import propagate_response
-from lavoro_library.models import LoginForm, RegistrationForm, Token, UserInDB
+
+# from lavoro_library.models import LoginForm, RegistrationForm, Token, UserInDB
+from lavoro_library.model.auth_api.db_models import Account
+from lavoro_library.model.auth_api.dtos import LoginDTO, RegisterDTO, TokenDTO
 
 
 def get_account(email: str):
     response = requests.get(f"http://auth-api/account/{email}")
-    return propagate_response(response, response_model=UserInDB)
+    return propagate_response(response, response_model=Account)
 
 
-def get_token(form_data: LoginForm):
+def get_token(form_data: LoginDTO):
     response = requests.post("http://auth-api/login/token", data=jsonable_encoder(form_data))
-    return propagate_response(response, response_model=Token)
+    return propagate_response(response, response_model=TokenDTO)
 
 
-def register_user(form_data: RegistrationForm):
+def register_user(form_data: RegisterDTO):
     response = requests.post("http://auth-api/register", data=jsonable_encoder(form_data))
     return propagate_response(response)
 
 
-def register_user_no_confirm(form_data: RegistrationForm):
+def register_user_no_confirm(form_data: RegisterDTO):
     response = requests.post("http://auth-api/register/no-confirm", data=jsonable_encoder(form_data))
     return propagate_response(response)
 
--- lavoro_api_gateway/routers/auth.py
+++ lavoro_api_gateway/routers/auth.py
@@ -5,27 +5,29 @@ from fastapi import APIRouter, Depends, Response
 from lavoro_api_gateway.dependencies.auth_dependencies import get_current_active_user
 from lavoro_api_gateway.helpers.auth_helpers import get_token, confirm_email_with_token, register_user
 
-from lavoro_library.models import LoginForm, RegistrationForm, Token, User
+# from lavoro_library.models import LoginForm, RegistrationForm, Token, User
+from lavoro_library.model.auth_api.db_models import Account
+from lavoro_library.model.auth_api.dtos import AccountDTO, LoginDTO, RegisterDTO, TokenDTO
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/login", response_model=Token)
-def login(response: Response, form_data: Annotated[LoginForm, Depends()]):
+@router.post("/login", response_model=TokenDTO)
+def login(response: Response, form_data: Annotated[LoginDTO, Depends()]):
     token_data = get_token(form_data)
     if token_data:
         response.set_cookie(key="access_token", value=f"Bearer {token_data.access_token}", httponly=True)
     return token_data
 
 
-@router.get("/account/current", response_model=User)
-def get_current_account(current_user: Annotated[User, Depends(get_current_active_user)]):
+@router.get("/account/current", response_model=AccountDTO)
+def get_current_account(current_user: Annotated[AccountDTO, Depends(get_current_active_user)]):
     return current_user
 
 
 @router.post("/register")
-def register(form_data: Annotated[RegistrationForm, Depends()]):
+def register(form_data: Annotated[RegisterDTO, Depends()]):
     return register_user(form_data)
 
 
